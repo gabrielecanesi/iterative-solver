c2942d827931948eaf0889eee66fecfa18661f6c
gianluca <g.ghislotti@campus.unimib.it>
2023-04-07 18:10:58 +0200

    JacobiUpdateStrategy.h added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "IterativeSolver.h"
 
 #include "GradientUpdateStrategy.h"
+#include "JacobiUpdateStrategy.h"
 #include "MatrixReader.h"
 
 typedef double precision;
@@ -11,13 +12,17 @@ int main() {
     Eigen::Matrix<precision, Eigen::Dynamic, 1> x(A.rows(), 1);
     x.setOnes();
     Eigen::Matrix<precision, Eigen::Dynamic, 1> b = A * x;
+    IterativeSolver<precision, Eigen::SparseMatrix<precision>> solver(5000);
 
-    auto strategy = new GradientUpdateStrategy<precision, Eigen::SparseMatrix<precision>>;
-    IterativeSolver<precision, Eigen::SparseMatrix<precision>> solver(5000, strategy);
-    auto foundSolution = solver.solve(A, b, 10e-10);
-
+    GradientUpdateStrategy<precision, Eigen::SparseMatrix<precision>> Gstrategy;
+    auto GfoundSolution = solver.solve(A, b, 10e-10, Gstrategy);
     std::cout << "Stopped after " << solver.neededIterations() << " iterations." << std::endl;
-    std::cout << "Gradient-based relative error: " << (foundSolution - x).squaredNorm() / x.squaredNorm() << std::endl;
-    
+    std::cout << "Gradient-based relative error: " << (GfoundSolution - x).squaredNorm() / x.squaredNorm() << std::endl;
+
+    JacobiUpdateStrategy<precision, Eigen::SparseMatrix<precision>> Jstrategy;
+    auto JfoundSolution = solver.solve(A, b, 10e-10, Jstrategy);
+    std::cout << "Stopped after " << solver.neededIterations() << " iterations." << std::endl;
+    std::cout << "Jacobi-based relative error: " << (JfoundSolution - x).squaredNorm() / x.squaredNorm() << std::endl;
+
     return 0;
 }
